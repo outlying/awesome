0c5bc1f928e9e79982ecbe2e4015d07d616fb6f4
outlying <outlying@gmail.com>
2014-09-21 18:25:11 +0200

    Remove nworking test

--- domain/src/test/java/com/antyzero/awesome/domain/log/LogParserTest.java
+++ domain/src/test/java/com/antyzero/awesome/domain/log/LogParserTest.java
@@ -1,30 +1,5 @@
 package com.antyzero.awesome.domain.log;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import java.io.InputStream;
-
-import static org.fest.assertions.api.Assertions.assertThat;
-
 public class LogParserTest {
 
-    public static final String VARNISH_LOG = "/varnish.log";
-
-    private InputStream inputStream;
-
-    @Before
-    public void setUp() throws Exception {
-
-        InputStream inputStream = LogParser.class.getResourceAsStream( VARNISH_LOG );
-
-        assertThat( inputStream ).isNotNull();
-    }
-
-    @Test
-    public void testRead() throws Exception {
-        //LogParser.read( inputStream );
-    }
 }
