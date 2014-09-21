b896ef9869ef74ef3832e659195a07d86239dabb
outlying <outlying@gmail.com>
2014-09-21 15:02:17 +0200

    Res loading on junit seems like big issue

--- domain/src/test/java/com/antyzero/awesome/domain/log/LogParserTest.java
+++ domain/src/test/java/com/antyzero/awesome/domain/log/LogParserTest.java
@@ -1,18 +1,32 @@
 package com.antyzero.awesome.domain.log;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class LogParserTest {
+
+    private InputStream inputStream;
+
+    @Before
+    public void setUp() throws Exception {
+
+        inputStream = getClass().getResourceAsStream( "/varnish" );
+
+        assertThat( inputStream ).isNotNull();
+    }
 
     @Test
     public void testRead() throws Exception {
-
-        InputStream inputStream = Thread.currentThread()
-                .getContextClassLoader()
-                .getResourceAsStream( "/varnish.log" );
-
         LogParser.read( inputStream );
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        inputStream.close();
     }
 }
