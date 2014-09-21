7b134565865e29ca1e56a4af0457a004934a58ee
outlying <outlying@gmail.com>
2014-09-21 11:32:41 +0200

    Regular expresion constructor

--- domain/src/test/java/com/antyzero/awesome/domain/log/EntryTest.java
+++ domain/src/test/java/com/antyzero/awesome/domain/log/EntryTest.java
@@ -1,11 +1,13 @@
 package com.antyzero.awesome.domain.log;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Test;
 
 import java.io.IOException;
 
 public class EntryTest {
 
+    @Test
     public void testEntryCreation() throws IOException {
 
         String logLine = IOUtils.toString( this.getClass().getResourceAsStream( "singleline.log" ), "UTF-8" );
