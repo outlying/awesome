7d584d7039cd9522fe66e5e660a1204e418b2472
outlying <outlying@gmail.com>
2014-09-23 08:10:43 +0200

    Adding RSS request

--- app/src/main/java/com/antyzero/awesome/network/AwesomeSpiceService.java
+++ app/src/main/java/com/antyzero/awesome/network/AwesomeSpiceService.java
@@ -7,8 +7,10 @@ import com.octo.android.robospice.persistence.CacheManager;
 import com.octo.android.robospice.persistence.exception.CacheCreationException;
 import com.octo.android.robospice.persistence.springandroid.json.jackson.JacksonObjectPersisterFactory;
 import com.octo.android.robospice.persistence.springandroid.json.jackson2.Jackson2ObjectPersisterFactory;
+import com.octo.android.robospice.persistence.springandroid.xml.SimpleSerializerObjectPersisterFactory;
 
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.feed.RssChannelHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
@@ -20,26 +22,36 @@ import java.util.List;
  */
 public class AwesomeSpiceService extends SpringAndroidSpiceService {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public CacheManager createCacheManager(Application application) throws CacheCreationException {
 
         CacheManager cacheManager = new CacheManager();
         cacheManager.addPersister(new Jackson2ObjectPersisterFactory(application));
 
+        // TODO will it work with RSS ?
+        cacheManager.addPersister(new SimpleSerializerObjectPersisterFactory(application));
+
         return cacheManager;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public RestTemplate createRestTemplate() {
 
         RestTemplate restTemplate = new RestTemplate();
 
         MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-        // TODO add atom/rss converter
+        RssChannelHttpMessageConverter rssConverter = new RssChannelHttpMessageConverter();
 
         final List<HttpMessageConverter<?>> listHttpMessageConverters = restTemplate.getMessageConverters();
 
         listHttpMessageConverters.add(jsonConverter);
+        listHttpMessageConverters.add(rssConverter);
 
         restTemplate.setMessageConverters(listHttpMessageConverters);
 
--- app/src/main/java/com/antyzero/awesome/ui/fragment/RssFragment.java
+++ app/src/main/java/com/antyzero/awesome/ui/fragment/RssFragment.java
@@ -1,7 +1,49 @@
 package com.antyzero.awesome.ui.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.antyzero.awesome.R;
+
 /**
  *
  */
-public final class RssFragment extends BaseFragment {
+public final class RssFragment extends BaseFragment implements AdapterView.OnItemClickListener {
+
+    private ListView listView;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
+        return inflater.inflate( R.layout.fragment_json, container, false );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onViewCreated( View view, Bundle savedInstanceState ) {
+
+        listView = (ListView) view.findViewById( R.id.listView );
+
+        // listView.setAdapter(  );
+        listView.setOnItemClickListener(this);
+    }
+
+    /**
+     * Move to RSS entry news
+     *
+     * {@inheritDoc}
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        // TODO add intent
+    }
 }
