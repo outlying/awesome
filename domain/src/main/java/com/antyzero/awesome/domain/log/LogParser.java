8e42248253d27c68431044f504f269bd92aa046c
outlying <outlying@gmail.com>
2014-09-24 00:40:25 +0200

    Another approach

--- domain/src/main/java/com/antyzero/awesome/domain/log/LogParser.java
+++ domain/src/main/java/com/antyzero/awesome/domain/log/LogParser.java
@@ -6,29 +6,41 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  */
 public class LogParser {
 
+    private static final String REGEX_LINE = "(\\d{1,3}(?:\\.\\d{1,3}){3}).+?(\\w+:\\/\\/.+?)\\s";
+
+    private static final Pattern PATTERN_LINE = Pattern.compile( REGEX_LINE );
 
     /**
      * Reads input stream of log file
      *
      * @throws java.io.IOException
      */
-    public static List<Entry> read( InputStream inputStream ) throws IOException {
+    public static void read( InputStream inputStream ) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line = reader.readLine();
 
-        List<Entry> logEntries = new ArrayList<>();
-
         while (line != null) {
-            logEntries.add( new Entry( line ) );
-        }
 
-        return logEntries;
+            Matcher matcher = PATTERN_LINE.matcher( line );
+
+            if( matcher.find() ){
+
+                String host = matcher.group( 1 );
+                String URL = matcher.group( 2 );
+
+            } else {
+                throw new IllegalArgumentException( "Invalid line format" );
+            }
+        }
     }
+
 }
